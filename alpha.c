e5c9a10cb19f0b1ad62cc9eede292e795a45c4b4
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-08-16 14:27:28 +0500

    Add files via upload
    alphabat from a to z using  while loop

---
+++ alpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char alphabet = 'a';
+
+    // print all alphabets from a to z
+    while (alphabet <= 'z') {
+        printf("%c ", alphabet);
+        alphabet++;
+    }
+
+    return 0;
+}
