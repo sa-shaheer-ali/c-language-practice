73916fe497fb0b567aa3339653605817d8d46a15
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-09-15 15:14:34 +0500

    Add files via upload
    printing array using loops

---
+++ arr1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int number(int arr[],int n);
+int main(){
+int arr[]={1,2,3,4,5,6};
+number(arr,6);
+
+return 0;
+}
+int number(int arr[],int n){
+    for (size_t i = 0; i < n; i++)
+    { printf("%d\t",arr[i]);
+        /* code */
+    }
+    
+   
+}
+
+ 
