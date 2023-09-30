5d0afdda5bb3f8109e5cb8ff5b28d72667db6868
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-10-01 01:26:41 +0500

    Add files via upload
    creating array to print array in  reverse.

--- reverse.c
+++ reverse.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
-
-
-int main() {
-    int n, i;
-    printf("Enter a positive integer n: ");
-    scanf("%d", &n);
-    i = n;
-    while (i >= 1) {
-        printf("%d ", i);
-        i--;
-
-    }
-
+int function(int arr[],int num);
+int print(int arr[],int num);
+int main (){
+    int arr[]={1,2,3,4,5};
+    function(arr,5);
+    print(arr,5);
     return 0;
 }
+int print(int arr[],int num){
+    for (int i = 0; i < num; i++)
+    {printf("%d\t",arr[i]);
 
+        /* code */
+    }
+    
+}
+int function(int arr[],int num){
+for (int i = 0; i< num/2; i++)
+{int firstnum=arr[i];
+int lastnum=arr[num-i-1];
+arr[i]=lastnum;
+arr[num-i-1]=firstnum;
+
+    /* code */
+}
+
+}
