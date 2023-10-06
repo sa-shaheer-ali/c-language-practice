5018c1f43c7a62d15fb951310c6d84658ff66a0b
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-10-06 22:26:02 +0500

    Add files via upload
    printing multiply character in the string

---
+++ string4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void string(char arr[]);
+int main (){
+    char str[100];
+    fgets(str ,100,stdin);
+    puts(str);
+    return 0l;
+
+}
+void string(char arr[]){
+    for (int i = 0; arr[i] < '\0'; i++)
+    {printf("%c",arr[i]);
+        /* code */
+    }
+    printf("\n");
+}
+// printing multiply character in the string
+
+
