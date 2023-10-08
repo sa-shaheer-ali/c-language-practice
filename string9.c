efa1e35715cc2f71e2743a6c7c7770a9fdc03b14
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-10-09 00:11:27 +0500

    Add files via upload
     copying string using strcpy

---
+++ string9.c
@@ -0,0 +1,12 @@
+// copying string using strcpy
+#include <stdio.h>
+#include<string.h>
+int main (){
+    char first[]="hello ";
+    char last[]= "world";
+    strcpy(first,last);
+    puts(first);
+
+   
+
+}
