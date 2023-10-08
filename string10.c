84be25c80d4f47781b39e7e47260521659ba901a
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-10-08 23:53:18 +0500

    Add files via upload
     joining strings by strcat

---
+++ string10.c
@@ -0,0 +1,12 @@
+// joining strings by strcat
+#include <stdio.h>
+#include <string.h>
+int main (){
+    char first[12]="barce";
+    char last[]="lona";
+    strcat(first,last);
+    puts(first);
+    return 0;
+
+
+}
