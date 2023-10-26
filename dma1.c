0351811bbafa6620aef2b58dd0620a382771e5d3
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-10-26 13:37:13 +0500

    Add files via upload
    using malloc in dynamic memory allocation

---
+++ dma1.c
@@ -0,0 +1,20 @@
+// using malloc in dynamic memory allocation
+#include <stdio.h>
+#include<stdlib.h>
+int main (){
+    int *ptr;
+    ptr =(int *) malloc(5*sizeof(int));
+    ptr[0]=1;
+    ptr[1]=2;
+    ptr[2]=3;
+    ptr[3]=4;
+    ptr[4]=5;
+    for (int i = 0; i <5 ; i++)
+    {printf("%d",ptr[i]);
+
+        /* code */
+    }
+    return 0;
+
+
+}
