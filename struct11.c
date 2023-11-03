cb11df990bb2033f41025be86673ced3de8b4ca8
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-11-03 11:27:57 +0500

    Add files via upload
    just simple code of printing int float and string in struct!

---
+++ struct11.c
@@ -0,0 +1,26 @@
+//just simple code of printing int float and string in struct
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ typedef struct newbe{
+    // int whatsup;
+    float feeling;
+    char urnamebruv[50];
+
+        /* data */
+    }soldier;
+int main (){
+   soldier s1;
+   int badboi=123;
+   float badthisis=123.2;
+   strcpy(s1.urnamebruv,"thorin");
+   printf("%d",badboi);
+
+   
+return 0;
+
+
+
+    
+
+}
