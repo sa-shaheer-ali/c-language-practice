4b5478aa7b7d4ecd0d0f2429d35540a19317a7c4
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-11-07 10:31:02 +0500

    Add files via upload
    printing arrays

--- arr1.c
+++ arr1.c
@@ -1,18 +1,9 @@
+// when i print it will print 7 because the squareroot will give 3 and in 3 number arr there is 7 so it will print 7.
 #include <stdio.h>
-int number(int arr[],int n);
-int main(){
-int arr[]={1,2,3,4,5,6};
-number(arr,6);
+#include <math.h>
+int main (){
+    int arr[]={1,2,3,7,5};
+    printf("%d",arr[(int)sqrt(9)]);
+    return 0;
 
-return 0;
 }
-int number(int arr[],int n){
-    for (size_t i = 0; i < n; i++)
-    { printf("%d\t",arr[i]);
-        /* code */
-    }
-    
-   
-}
-
- 
