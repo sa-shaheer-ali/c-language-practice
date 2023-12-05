8a559b61b4c24920d7998ff9f9a01ede7304be3a
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-12-06 01:30:53 +0500

    Add files via upload
    just a simple code of asking user favorite player and favorite club

---
+++ newworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    char player[40];
+    char club[50];
+
+
+    printf("enter your favourite player and the favourite club:");
+    scanf("%s",player);
+    scanf("%s",club);
+    printf("%s your fav player \n %s your fav club \n nice",player,club);
+    return 0;
+
+
+}
