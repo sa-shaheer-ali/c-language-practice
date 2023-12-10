0afbc131e7d5bfb34b1d4b9807b0669df7113de3
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-12-11 01:50:14 +0500

    Add files via upload
    using insertion sort in  c

---
+++ insertionsort.c
@@ -0,0 +1,43 @@
+/*in insertion sort numbers are sorted in a way for example if i have
+7654 so the first two will be compare so 6 goes befor 7 so 67 then
+again it will happen btw 7 and 5 so then 5 will move so 657
+thus 5 will not directly goes before 6 it will happen step by step*/
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
+
