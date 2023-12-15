927333cbd457785704b86b7e9760e1a693dbc72a
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2023-12-15 22:11:29 +0500

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void rectangle(int width,int  height){
+    for (int i = 0; i < height; i++)
+    {for (int j= 0; j <width; j++)
+    {printf("#");
+
+        /* code */
+    }
+
+printf("\n");
+        /* code */
+    }
+    
+}
+int main (){
+    // rectangle;
+    rectangle (7,3);
+   
+    printf("\n");
+    // for square;
+    rectangle(5,5);
+  
+    return 0;
+
+
+
+
+}
