8c2bdd5f06731156f0ed51f252774818fbf0ea5d
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2024-01-07 14:48:18 +0500

    Add files via upload
    finding weather the point is on origin or x axis or y axis

---
+++ posiofpoint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    float x, y;
+
+    // Input the coordinates of the point
+    printf("Enter the coordinates of the point (x y): ");
+    scanf("%f %f", &x, &y);
+if (x==0 &&y==0)
+{printf("the point is att origin");
+
+    /* code */
+}
+else if (x==0)
+{printf("the point is at y axis");
+
+    /* code */
+}
+else if (y==0)
+{printf("the point is at x axis");
+    /* code */
+}
+else {
+    printf("the pointer is a t neither");
+    
+}
+
+
+
+
+    return 0;
+}
